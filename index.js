58d402b0e1ae18dc37846c056e6fac60c355e3f4
suicao123 <chongtuanphat@gmail.com>
2025-06-22 12:03:17 +0700

    morgan

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    a = 2;
-    b = 1;
+app.use(morgan('combined'))
 
-    c = a + b;
+app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
